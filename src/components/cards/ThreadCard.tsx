3a9ebea8ab51388d1fd8660307c6870b52ffc80d
Johan <96794364+Joletuar@users.noreply.github.com>
2023-08-28 19:24:15 +0000

    Terminando la lógica para el perfil de usuario

--- src/components/cards/ThreadCard.tsx
+++ src/components/cards/ThreadCard.tsx
@@ -43,7 +43,11 @@ export const ThreadCard: FC<Props> = ({
   isComment,
 }) => {
   return (
-    <article className={`flex w-full flex-col rounded-xl  ${isComment ? "px-0 xs:px-7" : "bg-dark-2 p-7"}`}>
+    <article
+      className={`flex w-full flex-col rounded-xl  ${
+        isComment ? 'px-0 xs:px-7' : 'bg-dark-2 p-7'
+      }`}
+    >
       <div className='flex items-start justify-between'>
         <div className='flex w-full flex-1 flex-row gap-4'>
           <div className='flex flex-col items-center'>
@@ -68,7 +72,7 @@ export const ThreadCard: FC<Props> = ({
             </Link>
             <p className='mt-2 text-small-regular text-light-2'>{content}</p>
 
-            <div className='mt-5 flex flex-col gap-3'>
+            <div className={`mt-5 flex flex-col gap-3 ${isComment && 'mb-10'}`}>
               <div className='flex gap-3.5'>
                 <Image
                   src={'/assets/heart-gray.svg'}
