fd620098a26d43c65c0b1b68a17548d5d53996c6
Joletuar <joletuar@gmail.com>
2023-09-07 17:47:14 -0500

    Preparando para deploy

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -6,7 +6,6 @@ import { ClerkProvider } from '@clerk/nextjs';
 
 import { Topbar } from '@/components/shared/Topbar';
 import { LeftSidebar } from '@/components/shared/LeftSidebar';
-import { RightSidebar } from '@/components/shared/RightSidebar';
 import { Bottombar } from '@/components/shared/Bottombar';
 
 import '../globals.css';
@@ -40,8 +39,6 @@ const RootLayout: FC<Props> = ({ children }) => {
             <section className='main-container'>
               <div className='w-full max-w-4xl'>{children}</div>
             </section>
-
-            <RightSidebar />
           </main>
           <Bottombar />
         </body>
