1f4a19b955237c45ba88a2e5ff4744cb946d8c3d
Joletuar <joletuar@gmail.com>
2023-09-06 22:28:50 -0500

    Creando la página para realizar busquedas de usuarios, creación del action para buscar usuarios

---
+++ src/app/(root)/search/page.tsx
@@ -0,0 +1,55 @@
+import { redirect } from 'next/navigation';
+
+import { currentUser } from '@clerk/nextjs';
+
+import { fetchUser, fetchUsers } from '@/lib/actions/user.actions';
+import { UserCard } from '@/components/cards/UserCard';
+
+const SearchPage = async () => {
+  // usuario que esta logeado con clerk
+  const user = await currentUser();
+
+  if (!user) return null;
+
+  // información del perfil del usuario que estamos visitando
+  const userInfo = await fetchUser(user.id);
+
+  if (!userInfo?.onboarded) redirect('/onboarding');
+
+  // obtener usuarios
+  const { users, isNext } = await fetchUsers({
+    userId: user.id,
+    searchString: '',
+    pageNumber: 1,
+    pageSize: 25,
+  });
+
+  return (
+    <section>
+      <h1 className='head-text mb-10'>Search</h1>
+
+      {/* Search bar */}
+
+      <div className='mt-14 flex flex-col gap-9'>
+        {users.length === 0 ? (
+          <p className='no-result'>Usuario no encontrado</p>
+        ) : (
+          <>
+            {users.map((user) => (
+              <UserCard
+                key={user.id}
+                id={user.id}
+                name={user.name}
+                username={user.username}
+                imgUrl={user.imgUrl}
+                personType='User'
+              />
+            ))}
+          </>
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default SearchPage;
--- src/lib/actions/user.actions.ts
+++ src/lib/actions/user.actions.ts
@@ -3,6 +3,7 @@
 import { revalidatePath } from 'next/cache';
 
 import User from '@/lib/models/user.model';
+import { FilterQuery, SortOrder } from 'mongoose';
 
 import { connectToDB } from '../mongoose';
 import Thread from '../models/thread.models';
@@ -99,5 +100,61 @@ export async function fetchUserPosts(userId: string) {
     return postsUser;
   } catch (error: any) {
     throw new Error(`Failed to get user posts: ${error?.message}`);
+  }
+}
+
+export async function fetchUsers({
+  userId,
+  pageNumber = 1,
+  pageSize = 20,
+  searchString = '',
+  sortBy = 'desc',
+}: {
+  userId: string;
+  pageNumber?: number;
+  pageSize?: number;
+  searchString?: string;
+  sortBy?: SortOrder;
+}) {
+  await connectToDB();
+
+  const offset = (pageNumber - 1) * pageSize;
+
+  // regrex para buscar en el nombre del usuario sin tener en cuenta minúsculas o mayúsculas
+  const regex = new RegExp(searchString, 'i');
+
+  // query para filtrar los usuarios que no sean el usuario actual
+  const query: FilterQuery<typeof User> = { id: { $ne: userId } };
+
+  if (searchString.trim() !== '') {
+    // si se especifica una cadena de busqueda, se agrega un filtro para buscar en el nombre o username del usuario
+    // con el operador $regex, se indica que la busqueda sea case-insensitive (no importa si es mayáscula o mináscula)
+    // $regex: /regex/, $options: 'i'
+
+    query.$or = [{ username: { $regex: regex } }, { name: { $regex: regex } }];
+  }
+
+  // se agrega un sort para ordenar los usuarios por fecha de creación
+  const sortOption = { createdAt: sortBy };
+
+  try {
+    // obtenemos los usuarios con la query y la opcion de ordenamiento
+    const usersQuery = await User.find(query)
+      .skip(offset)
+      .limit(pageSize)
+      .sort(sortOption);
+
+    // obtener el número total de usuarios
+    const totalUserCount = await User.countDocuments(query);
+
+    // determinamos si tenemos o no siguiente página
+    const isNext = totalUserCount > offset + usersQuery.length;
+
+    return {
+      users: usersQuery,
+      isNext,
+    };
+  } catch (error: any) {
+    throw new Error(`Failed to get users: ${error?.message}`);
   }
 }
